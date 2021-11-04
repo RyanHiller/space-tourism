0a8462bfb0bfd3f34fe04b7b3bd4ea7d4986222b
Ryan Hiller <ryanhillerdev@gmail.com>
2021-11-04 10:51:36 -0600

    Technology layout groundwork

--- src/pages/Technology/Technology.js
+++ src/pages/Technology/Technology.js
@@ -11,7 +11,30 @@ const Technology = () => {
     layout.classList.remove('destinationBG')
   }, [])
 
-  return <div className={styles.Technology}></div>
+  const technology = {}
+
+  return (
+    <div className={styles.Technology}>
+      <h5>
+        <span>03</span>Space launch 101
+      </h5>
+      {technology.image}
+      <ol className={styles.Navigation}>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ol>
+      <span className={`sh2`}>The terminology...</span>
+      <h3 className={styles.Name}>{technology.name}Launch Vehicle</h3>
+      <p className={styles.Description}>
+        {technology.description}A launch vehicle or carrier rocket is a
+        rocket-propelled vehicle used to carry a payload from Earth's surface to
+        space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the
+        most powerful in operation. Standing 150 metres tall, it's quite an
+        awe-inspiring sight on the launch pad!
+      </p>
+    </div>
+  )
 }
 
 export default Technology
